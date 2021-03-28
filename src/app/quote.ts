2e2d770a2204392ce6a706d6854081608dfe4f4d
louisenje <louisenje@gmail.com>
2021-03-28 15:48:16 +0300

    created a contractor that initializes logic for creating an object

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -2,3 +2,12 @@ export class quote {
     PersonSubmitted:boolean;
   name: any;
 }
+constructor(
+    public quote:string,
+    public author:string,
+    public personSubmitted:string,
+    public upvote?:number,
+    public downvote?:number,
+    public entryDate?:Date,
+    public entryTime?:Time
+    )
