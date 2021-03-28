dbfdb101dc39d45d54989256c28a5a2fced50769
louisenje <louisenje@gmail.com>
2021-03-28 16:41:05 +0300

    added the quotes that will display on the quote app

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,10 +8,10 @@ import{Quote} from '../quote'
 })
 export class QuoteComponent implements OnInit {
   quotes:Quote[]=[
-    new Quote("There is power in believing","Nasseh","Lenny",0,0,new Date(2020,0,2)),
-    new Quote("Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.","Albert Einstein","Nemo",0,0,new Date(2020,1,2)),
-    new Quote("Same today and yesterday","Ndungu","Kiba",0,0,new Date(2020,2,2)),
-    new Quote("So many books, so little time ","Frank Zappa","Alex",0,0,new Date(2020,3,2))
+    new Quote("You must be the change you want to see in the world","Mahatma Gandhi","Louise",0,0,new Date(2020,0,2)),
+    new Quote("Money is not the answer, but it makes the diffrence.","Albert Einstein","Nemo",0,0,new Date(2020,1,2)),
+    new Quote("You cant stop my happiness, coz i like the way I am.","Edna Turnbad","Luna",0,0,new Date(2020,2,2)),
+    new Quote("Everything you can imagine is real ","Pablo Picaso","Alex",0,0,new Date(2020,3,2))
 
   ];
 
