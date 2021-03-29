6473ff1a7d2ff8529295ce1607b709c9af3335d2
louisenje <louisenje@gmail.com>
2021-03-29 05:13:33 +0300

    added @Hostlistener to highlight

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,22 @@
 import { Directive } from '@angular/core';
+import{Quote} from '../quote'
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
+constructor(private elem:ElementRef) { }
+ 
+quote: Quote
+@HostListener('mouseenter') onMouseEnter() {
+    
+  this.highlight('yellow');
+}
+@HostListener('mouseleave') onMouseLeave() {
+  this.highlight(null);
+}
 
-  constructor() { }
-
+private highlight(color: string) {
+  this.elem.nativeElement.style.backgroundColor = color;
+}
 }
