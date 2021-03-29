975019508188e09bf1c29438ce94dcf1189d50c8
louisenje <louisenje@gmail.com>
2021-03-29 05:44:33 +0300

    created a form method component and assigned submitQuote

--- src/app/formcomponent/formcomponent.component.ts
+++ src/app/formcomponent/formcomponent.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import{Quote} from '../quote'
 
 @Component({
@@ -11,6 +11,12 @@ export class FormcomponentComponent implements OnInit {
   newQuote=new Quote("","","",0,0,new Date())
   @Output() addQuote = new EventEmitter<Quote>();
 
+
+  submitQuote(){
+    this.addQuote.emit(this.newQuote);
+    this.newQuote=new Quote("","","",0,0,new Date())
+      }
+      
   constructor() { }
 
   ngOnInit(): void {
